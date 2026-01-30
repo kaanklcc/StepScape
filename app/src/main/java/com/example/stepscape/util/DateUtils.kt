e32f764d34c98cd4261b917847464fe8dd4e6e9a
Kaan Kılıç <kan.klc.1903@gmail.com>
2026-01-30 23:10:31 +0300

    date opeartions class added

---
+++ app/src/main/java/com/example/stepscape/util/DateUtils.kt
@@ -0,0 +1,30 @@
+package com.example.stepscape.util
+
+import java.util.Calendar
+
+/**
+ * Tarih işlemlerini çevirme sınııfı.
+ */
+object DateUtils {
+
+
+    fun getTodayStartMillis(): Long {
+        return Calendar.getInstance().apply {
+            set(Calendar.HOUR_OF_DAY, 0)
+            set(Calendar.MINUTE, 0)
+            set(Calendar.SECOND, 0)
+            set(Calendar.MILLISECOND, 0)
+        }.timeInMillis
+    }
+
+
+    fun getDaysAgoMillis(days: Int): Long {
+        return Calendar.getInstance().apply {
+            add(Calendar.DAY_OF_YEAR, -days)
+            set(Calendar.HOUR_OF_DAY, 0)
+            set(Calendar.MINUTE, 0)
+            set(Calendar.SECOND, 0)
+            set(Calendar.MILLISECOND, 0)
+        }.timeInMillis
+    }
+}
